498fff27e0ce22947fae7cb1e465be13827dcd86
Fran Lucena <Fran@Fran-MacBook-Pro.local>
2016-10-26 20:04:59 +0200

    Added config

---
+++ config/local_config.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Fran on 25/10/16.
+ */
+
+"use strict";
+
+module.exports = {
+    
+    platform: ['ios', 'android'],
+    languages: ['en', 'es'],
+    
+    jwt: {
+        secret: 'fspsefinds1235fdsfsacc14453tfdv34'
+    }
+};
